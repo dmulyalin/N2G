827a0353026ca78e8844f1626b0211cb85d35db1
denis <d.mulyalin@gmail.com>
2020-08-22 20:37:44 +1000

    updated version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Denis Mulyalin'
 author = 'Denis Mulyalin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.2'
 
 # use index.rst instead of contents.rst:
 master_doc = 'index'
