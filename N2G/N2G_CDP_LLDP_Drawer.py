f94dc5e113a2901f57d64162f9120134d4af8b73
Denis <d.mulyalin@gmail.com>
2020-09-28 07:08:05 +1000

    some

--- N2G/N2G_CDP_LLDP_Drawer.py
+++ N2G/N2G_CDP_LLDP_Drawer.py
@@ -43,150 +43,6 @@ Interface: {{ src_label | resuball(IfsNormalize) }},  Port ID (outgoing port): {
 </template>
 """
 
-Cisco_NXOS = """
-<template name="Cisco_NXOS" results="per_template">
-
-<vars name="vars">
-local_hostname="gethostname"
-</vars>
-
-<input name="Cisco_NX-OS" load="yaml">
-url: "./Cisco NXOS/"
-</input>
-
-<g name="interfaces**.{{ Interface }}.cdp" input="Cisco_NX-OS">
-##===================-CDP-===================
-
-## Cisco new NX-OS:
-Device ID: {{ peer_hostname | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}
-Device ID:{{ peer_hostname | _start_ | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}
-  IP address: {{ peer_ip | default }}
-##with space at the end:
-  IP address: {{ peer_ip | default }}    
-Platform: {{ peer_platform | ORPHRASE | exclude("-VM-") }},  Capabilities: {{ peer_capabilities | ORPHRASE | truncate(2) }}
-##with space at the end:
-Platform: {{ peer_platform | ORPHRASE | exclude("-VM-") }},  Capabilities: {{ peer_capabilities | ORPHRASE | truncate(2) }} 
-Interface: {{ Interface | resuball(IfsNormalize) }},  Port ID (outgoing port): {{ peer_interface | ORPHRASE | resuball(IfsNormalize) }}
-
-##old Cisco N7K:
-    IPv4 Address: {{ peer_ip | default }}
-{{ peer_platform | ORPHRASE | exclude("-VM-") }}, Capabilities: {{ peer_capabilities | ORPHRASE | truncate(2) }}
-{{ Interface | resuball(IfsNormalize) }}, Port ID (outgoing port): {{ peer_interface | resuball(IfsNormalize) | ORPHRASE }}
-</g >
-
-<g name="interfaces**.{{ Interface }}.lldp" input="Cisco_NX-OS">
-##===================-LLDP-===================
-Chassis id: {{ ignore }}
-Port id: {{ peer_interface | ORPHRASE | resuball(IfsNormalize) | exclude(vmnic) | default }}
-Local Port id: {{ Interface | resuball(IfsNormalize) }}
-System Name: {{ peer_hostname | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}
-System Description: {{ peer_platform | ORPHRASE | exclude("-VM-") | truncate(2) }}
-Management Address: {{ peer_ip | default }}
-</g>
-
-<g name="interfaces**.{{ Interface }}.cfg" input="Cisco_NX-OS">
-##===================-CFG-===================
-interface {{ Interface | resuball(IfsNormalize) }}
-  description {{ description | ORPHRASE }}
-  switchport mode trunk {{ mode | set(trunk) }}
-  switchport trunk native vlan {{ accessVlan }}
-  switchport access vlan {{ accessVlan }}
-  switchport trunk allowed vlan {{ trunkVlans | joinmatches() | unrange('-',',') }}
-  switchport trunk allowed vlan add {{ trunkVlans | joinmatches() | unrange('-',',') }}
-  channel-group {{ lag }} mode {{ lag_mode }}
-  vpc {{ vPC_ID }}
-  mpls ip {{ mpls | set(Enabled) }}
-  mtu {{ mtu }}
-  ip address {{ ipv4 }}/{{ maskv4 }}
-  ipv6 address {{ ipv6 }}/{{ maskv6 }}
-  ip ospf cost {{ ospf_cost}}
-  ip ospf network point-to-point {{ ospf_link_type | set(ptp) }}
-  ip router ospf {{ ospf_pid }} area {{ ospf_area }}
-  vrf member {{ vrf }}
-!{{ _end_ }}
-</g>
-</template>
-"""
-
-Cisco_IOSXR = """
-<vars name="vars">
-local_hostname="gethostname"
-</vars>
-
-<input name="Cisco_IOS-XR" load="yaml">
-url: "./Cisco/IOS-XR/"
-</input>
-
-<g name="interfaces**.{{ Interface }}.cdp" input="Cisco_IOS-XR">
-##===================-CDP-===================
--------------------------{{ _start_ }}
-Device ID: {{ peer_hostname | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}
-  IPv4 address: {{ peer_ip | default }}
-Platform: {{ peer_platform | ORPHRASE | exclude("-VM-") }},  Capabilities: {{ peer_capabilities | ORPHRASE | truncate(2) }}
-##with space at the end:
-Platform: {{ peer_platform | ORPHRASE | exclude("-VM-") }},  Capabilities: {{ peer_capabilities | ORPHRASE | truncate(2) }}  
-Interface: {{ Interface | resuball(IfsNormalize) }}
-Port ID (outgoing port): {{ peer_interface | resuball(IfsNormalize) | ORPHRASE }}
-</g>
-
-<g name="interfaces**.{{ Interface }}.cfg" input="Cisco_IOS-XR">
-##===================-CFG-===================
-interface {{ Interface | resuball(IfsNormalize) }}
- description {{ description | ORPHRASE }}
- mtu {{ mtu }}
- ipv4 address {{ ipv4 | exclude(":") }} {{ maskv4 }}
- ipv6 address {{ ipv6 | contains(":") }}/{{ maskv6 }}
-## to match " ipv6 address bla/64 eui-64" v6 ips:
- ipv6 address {{ ipv6 | contains(":") }} {{ maskv6 | contains(eui) }}
- vrf {{ vrf }}
- bundle id {{ lag }} mode {{ lag_mode }}
-!{{ _end_ }}
-</g>
-"""
-
-Huawei = """
-<vars name="vars">
-local_hostname="gethostname"
-</vars>
-
-<input name="Huawei" load="yaml">
-url: "./Huawei/"
-</input>
-
-<g name="interfaces**.{{ Interface }}.lldp" input="Huawei">
-##===================-LLDP-===================
-{{ Interface | resuball(IfsNormalize) }}  has 1 neighbor(s):
-Port ID                            :{{ peer_interface | ORPHRASE | resuball(IfsNormalize) | exclude(vmnic)}}
-System name                        :{{ peer_hostname | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}    
-##no spaces at the end:  
-System name                        :{{ peer_hostname | replaceall(domainsToStrip) | resub(r"\\(\\S+\\)", "") }}
-System description                 :{{ peer_platform | ORPHRASE | exclude("-VM-") | truncate(2) }}
-Management address                 :{{ peer_ip | default }}
-</g>
-
-<g name="interfaces**.{{ Interface }}.cfg" input="Huawei">
-##===================-CFG-===================
-interface {{ Interface | resuball(IfsNormalize) }}
- description {{ description | ORPHRASE }}
- port link-type trunk {{ mode | set(trunk) }}
- port trunk pvid vlan {{ accessVlan }}
- port trunk allow-pass vlan {{ trunkVlans | ORPHRASE | unrange('to',' ') | replace(' ', ',') | joinmatches() }}
- port trunk allow-pass vlan all {{ trunkVlans | set(ALL) }}
- dfs-group 1 m-lag {{ mLAG_ID }}
- eth-trunk {{ lag }}
- peer-link 1 {{ peer_link | set(True) }}
- ip address {{ ipv4 }} {{ maskv4 }}
- ospf cost {{ ospf_cost}}
- ospf network-type p2p {{ ospf_link_type | set(ptp) }}
- ospf enable {{ ospf_pid }} area {{ ospf_area }}
- mpls {{ mpls | set(Enabled) }}
- ip binding vpn-instance {{ vrf }}
-##with spaces at the end:
- eth-trunk {{ lag }}   
-#{{ _end_ }}
-</g>
-"""
-
 
 #=============================================================================
 # COMMON TTP PARSER Variables:                                           
@@ -290,6 +146,7 @@ class cdp_lldp_drawer():
         
         # work:
         self._parse(data)
+		self._process()
         self._populate_drawing()
 
 
@@ -335,6 +192,10 @@ class cdp_lldp_drawer():
         self.parsed_data = parser.result(structure="flat_list")
 
 
+	def _process(self):
+		pass
+		
+		
     def _populate_drawing(self):
         for item in self.parsed_data:
             self.drawing.from_list(item.get("cdp_peers", [{}]))
