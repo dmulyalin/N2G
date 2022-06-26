840de0016e5702e12da376a08c0cfc827c0e25b1
Denis <d.mulyalin@gmail.com>
2022-06-26 20:19:58 +1000

    red the docs documentation configuration fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Denis Mulyalin'
 author = 'Denis Mulyalin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 # use index.rst instead of contents.rst:
 master_doc = 'index'
